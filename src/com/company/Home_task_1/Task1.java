06b1d2a9dcb89ebbc5a6b86488f9bfdde394ebc8
alexa <alexandergostilo@gmail.com>
2021-09-05 14:22:27 +0300

    style: add comments

--- src/com/company/Home_task_1/Task1.java
+++ src/com/company/Home_task_1/Task1.java
@@ -2,7 +2,6 @@ package com.company.Home_task_1;
 
 /**
  * Задание 1.
- *
  * Есть некоторый массив, состоящий из целых чисел, также есть некоторое
  * число "n". Суть задания посчитать сумму всех элементов массива, однако если
  * в массиве встречается число "n" то в сумму его включать не нужно.
@@ -10,6 +9,7 @@ package com.company.Home_task_1;
 public class Task1 {
 
     public static void main(String[] args) {
+        // the array below acts as "some kind of array"
         int[] array = {1, 2, 3, 5, 8, 13};
         int sum = 0;
         int n = 8;
---
+++ src/com/company/Home_task_1/Task4.java
@@ -0,0 +1,2 @@
+package com.company.Home_task_1;public class Task4 {
+}
