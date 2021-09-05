f254d1bebd1a7397911c62d2257397df13c27656
alexa <alexandergostilo@gmail.com>
2021-09-05 13:49:47 +0300

    solved task 3

---
+++ src/com/company/Home_task_1/Task3.java
@@ -0,0 +1,19 @@
+package com.company.Home_task_1;
+
+/**
+ * Задание 3
+ * Дан некоторый массив, суть задания инвертировать его.
+ */
+public class Task3 {
+
+    public static void main(String[] args) {
+        // the array below acts as "some kind of array"
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+}
