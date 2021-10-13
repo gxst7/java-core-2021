a55106e756cfa4a3e79aca36fac6c26586b81941
Alexander Gostilo <alexandergostilo@gmail.com>
2021-10-13 18:25:26 +0300

    build: add task 1 from home work 5

--- src/com/company/Home_Work_5/Task_1.java
+++ src/com/company/Home_Work_5/Task_1.java
@@ -1,12 +1,24 @@
 package com.company.Home_Work_5;
 
+import java.io.*;
+
 /**
  * Необходимо реализовать программу, которая будет считывать
  * строки, введенные пользователем и выводить их в консоль.
  */
 public class Task_1 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+        String str;
+        pw.println("Enter some strings of text");
+        pw.println("Enter 'stop' to finish");
+        do {
+            str = br.readLine();
+            if (!str.equals("stop")) {
+                pw.println(str);
+            }
+        } while (!str.equals("stop"));
     }
 }
