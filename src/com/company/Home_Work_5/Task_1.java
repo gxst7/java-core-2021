a327f8f621aa1a78d71d10eefa7eaf16f25ce242
Alexander Gostilo <alexandergostilo@gmail.com>
2021-10-13 18:02:47 +0300

    almost add task1

---
+++ src/com/company/Home_Work_5/Task_1.java
@@ -0,0 +1,12 @@
+package com.company.Home_Work_5;
+
+/**
+ * Необходимо реализовать программу, которая будет считывать
+ * строки, введенные пользователем и выводить их в консоль.
+ */
+public class Task_1 {
+
+    public static void main(String[] args) {
+
+    }
+}
